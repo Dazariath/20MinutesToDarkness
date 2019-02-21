1fa5df65dd3a728463e5b74e116cb731122d46de
Eibho <eibhyor@gmail.com>
2019-02-21 22:04:05 +0000

    Day and Night
    Press the up and down arrow keys to change between day and night

---
+++ 20MinutesToDark/Assets/Scripts/EnableDisable.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableDisable : MonoBehaviour
+{
+    public GameObject dayObject;
+    public GameObject nightObject;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputDetection();
+
+    }
+
+    void InputDetection()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            Debug.Log("deactivate");
+            dayObject.SetActive(false);
+            nightObject.SetActive(true);
+        }
+
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            dayObject.SetActive(true);
+            nightObject.SetActive(false);
+        }
+    }
+}
