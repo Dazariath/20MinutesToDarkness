9aa9eeceba1274a8357d80989fc2a982030c39c2
Eibho <eibhyor@gmail.com>
2019-02-28 16:37:04 +0000

    Nothing

---
+++ 20MinutesToDark/Assets/Scripts/Respawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+   
+    public Transform respawnPoint;
+    
+   
+
+     void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("Respawn");
+            other.transform.position = respawnPoint.position;
+        }
+
+
+    }
+}
