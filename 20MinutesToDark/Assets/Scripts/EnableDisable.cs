b119692237dba390f33e2ee31eaed368222f9aa2
Eibho <eibhyor@gmail.com>
2019-03-09 14:49:54 +0000

    Added a cooldown

--- 20MinutesToDark/Assets/Scripts/EnableDisable.cs
+++ 20MinutesToDark/Assets/Scripts/EnableDisable.cs
@@ -8,34 +8,54 @@ public class EnableDisable : MonoBehaviour
     public GameObject nightObject;
     private bool nightObjectBool;
     private bool dayObjectBool;
+
+    public float coolDown = 2;
+    private float coolDownTimer;
     
     void Start()
     {
         nightObjectBool = true;
         dayObjectBool = false;
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        InputDetectionLeftShift();
-        InputDetectionRightShift();
+        if (coolDownTimer >= 0)
+        {
+            coolDownTimer -= Time.deltaTime;
+        }
 
+        if(coolDownTimer < 0 )
+        {
+            coolDownTimer = 0;
+        }
+
+        if(coolDownTimer == 0)
+        {
+            InputDetectionLeftShift();
+            InputDetectionRightShift();
+            
+        }
     }
+    
 
-    void InputDetectionLeftShift()
+   void InputDetectionLeftShift()
     {
         if(Input.GetKeyDown(KeyCode.LeftShift))
         {
+            coolDownTimer = coolDown;
+
             Debug.Log("Left Shift");
-            if(dayObjectBool == true)
+            if(dayObjectBool == true )
             {
                 dayObjectBool = false;
                 nightObjectBool = true;
                 dayObject.SetActive(false);
                 nightObject.SetActive(true);
             }
-            else 
+            else if (dayObjectBool == false)
             {
                 dayObjectBool = true;
                 nightObjectBool = false;
@@ -48,17 +68,20 @@ public class EnableDisable : MonoBehaviour
 
     void InputDetectionRightShift()
     {
+       
         if (Input.GetKeyDown(KeyCode.RightShift))
         {
+            coolDownTimer = coolDown;
+
             Debug.Log("Right Shift");
-            if (dayObjectBool == true)
+            if (dayObjectBool == true )
             {
                 dayObjectBool = false;
                 nightObjectBool = true;
                 dayObject.SetActive(false);
                 nightObject.SetActive(true);
             }
-            else
+            else if(dayObjectBool == false)
             {
                 dayObjectBool = true;
                 nightObjectBool = false;
@@ -67,4 +90,5 @@ public class EnableDisable : MonoBehaviour
             }
         }
     }
+    
 }
