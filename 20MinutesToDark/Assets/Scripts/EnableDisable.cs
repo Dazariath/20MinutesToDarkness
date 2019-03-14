671c74225ab4d8908da08ac40418a74ce5eec9d9
Eibho <eibhyor@gmail.com>
2019-03-14 14:40:20 +0000

    Timer

--- 20MinutesToDark/Assets/Scripts/EnableDisable.cs
+++ 20MinutesToDark/Assets/Scripts/EnableDisable.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DentedPixel;
 
 public class EnableDisable : MonoBehaviour
 {
@@ -12,17 +13,23 @@ public class EnableDisable : MonoBehaviour
 
     public float coolDown = 2;
     private float coolDownTimer;
+
+    public GameObject bar;
+    public GameObject timer;
+    public int time =5;
     
     void Start()
     {
         nightObjectBool = true;
         dayObjectBool = false;
         sun.SetActive(false);
+     //   timer.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if (coolDownTimer >= 0)
         {
             coolDownTimer -= Time.deltaTime;
@@ -46,58 +53,74 @@ public class EnableDisable : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Q))
         {
-            
-
-            Debug.Log("Q");
-            //GO TO NIGHT
-            if(dayObjectBool == true )
+            if (dayObjectBool == false)
             {
-                dayObjectBool = false;
-                nightObjectBool = true;
-                dayObject.SetActive(false);
-                nightObject.SetActive(true);
-                sun.SetActive(false);
-                coolDownTimer = coolDown;
-            }
-            //GO TO DAY
-            else if (dayObjectBool == false)
-            {
+                AnimateBar();
                 dayObjectBool = true;
                 nightObjectBool = false;
                 dayObject.SetActive(true);
                 nightObject.SetActive(false);
-                //canvas.SetActive(true);
-            }  
-        }
-
-    }
-
-    void InputDetectionRightShift()
-    {
-       
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            
-
-            Debug.Log("Click");
-            if (dayObjectBool == true )
-            {
-                dayObjectBool = false;
-                nightObjectBool = true;
-                dayObject.SetActive(false);
-                nightObject.SetActive(true);
-                coolDownTimer = coolDown;
-                sun.SetActive(false);
-            }
-            else if(dayObjectBool == false)
-            {
-                dayObjectBool = true;
-                nightObjectBool = false;
-                dayObject.SetActive(true);
-                nightObject.SetActive(false);
-              //  canvas.SetActive(true);
+                timer.SetActive(true);
+                StartCoroutine(AnimateBar());
+                StartCoroutine(ChangeToNight());
             }
         }
     }
     
+    void InputDetectionRightShift()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+           
+            Debug.Log("Click");
+         
+            if(dayObjectBool == false)
+            {
+                AnimateBar();
+                dayObjectBool = true;
+                nightObjectBool = false;
+                dayObject.SetActive(true);
+                nightObject.SetActive(false);
+                timer.SetActive(true);
+                StartCoroutine(AnimateBar());
+               
+                StartCoroutine(ChangeToNight());
+            }
+        }
+    }
+
+    IEnumerator ChangeToNight()
+    {
+        
+        yield return new WaitForSeconds(5);
+
+            dayObjectBool = false;
+            nightObjectBool = true;
+            dayObject.SetActive(false);
+            nightObject.SetActive(true);
+            coolDownTimer = coolDown;
+            sun.SetActive(false);
+          //  timer.SetActive(false);
+        
+    }
+    
+    IEnumerator AnimateBar ()
+    {
+        int scale = 0;
+        LeanTween.scaleX(bar, 0, time);
+        yield return new WaitForSeconds(5);
+        scale = 1;
+        LeanTween.scaleX(bar, 1,time);
+
+    }
 }
+/*  if (dayObjectBool == true )
+           {
+               dayObjectBool = false;
+               nightObjectBool = true;
+               dayObject.SetActive(false);
+               nightObject.SetActive(true);
+               coolDownTimer = coolDown;
+               sun.SetActive(false);
+           }
+           */
