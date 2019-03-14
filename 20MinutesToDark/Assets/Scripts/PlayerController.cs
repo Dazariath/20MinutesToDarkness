75634f8e61fbcce50823dffc586ddcfef1ae3daf
Eibho <eibhyor@gmail.com>
2019-03-14 11:33:33 +0000

    Added sprint (Q key for now)

--- 20MinutesToDark/Assets/Scripts/PlayerController.cs
+++ 20MinutesToDark/Assets/Scripts/PlayerController.cs
@@ -3,7 +3,7 @@
 public class PlayerController : MonoBehaviour
 {
 
-    [SerializeField] private float speed = 5f;
+    private float speed = 5f;
     [SerializeField] private float lookSensitivity = 3f;
 
     private PlayerMove movePlayer;
@@ -16,6 +16,13 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //movement
+        if (Input.GetKey(KeyCode.Q))
+        {
+            speed = 10f;
+        }
+        else
+            speed = 5;
+        
         float xMove = Input.GetAxisRaw("Horizontal");
         float zMove = Input.GetAxisRaw("Vertical");
 
@@ -25,7 +32,7 @@ public class PlayerController : MonoBehaviour
         Vector3 velocity = (moveHorizonatal + moveVertical).normalized * speed;
 
         movePlayer.Move(velocity);
-
+     
         //Player Rotation
         float yRot = Input.GetAxisRaw("Mouse X");
         Vector3 _rotation = new Vector3(0f, yRot, 0f) * lookSensitivity;
